f5f52ee0ceaf11b4b94f4e1f478f153db4636c1c
Samantha Diaz <samidiaz3@gmail.com>
2022-01-14 13:27:46 -0500

    commit tree 016fab6aa4e1b16f96158b725c16a3a2465ec486

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// 1) no longer has DOM node 'main#main'
+document.querySelector("main").remove();
+
+// 2) has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+newHeader.className = "victory"
+// let newHeader = document.querySelector("h1#victory")
+newHeader.innerHTML = "SAM is the champion"
+
+// 4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
